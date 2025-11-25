91d3309c06ef631de64e3514160bde1c90478514
Deivd730 <davidcastill2005@gmail.com>
2025-11-25 16:42:58 +0100

    #2 Refactor film listing methods and update routes for genre and year filtering

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\FilmController;
-use App\Http\Middleware\ValidateYear;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|   
+|
 */
 
 Route::get('/', function () {
@@ -22,10 +21,9 @@ Route::get('/', function () {
 Route::middleware('year')->group(function () {
     Route::group(['prefix' => 'filmout'], function () {
         // Routes included with prefix "filmout"
-        Route::get('oldFilms/{year?}', [FilmController::class, "listOldFilms"])->name('oldFilms');
-        Route::get('newFilms/{year?}', [FilmController::class, "listNewFilms"])->name('newFilms');
-        Route::get('films/{year?}/{genre?}', [FilmController::class, "listFilms"])->name('listFilms');
+        Route::get('oldFilms/{year?}', [FilmController::class, 'listOldFilms'])->name('oldFilms');
+        Route::get('newFilms/{year?}', [FilmController::class, 'listNewFilms'])->name('newFilms');
+        Route::get('filmsByYear/{year?}', [FilmController::class, 'listFilmsByYear'])->name('listFilmsByYear');
+        Route::get('filmsByGenre/{genre?}', [FilmController::class, 'listFilmsByGenre'])->name('listFilmsByGenre');
     });
 });
-
-
